ef0b26caf992629de067a7634e67090a3c2daf1b
Butosolala <tianpei.lei@ni.com>
2023-01-05 05:58:08 -0800

    separate parser

--- exampleserver.cpp
+++ exampleserver.cpp
@@ -6,40 +6,52 @@
 #include <stdlib.h>
 #include <string>
 #include <boost/process.hpp>
+#include <boost/thread.hpp>
 #include "message.hpp"
 
-void start()
+class Server
 {
-  std::string command = "./Prog ";
-  boost::process::ipstream is;
-  boost::process::child c(command.c_str(), boost::process::std_out > is);
 
-  c.detach();
-
-  boost::interprocess::message_queue::remove ("Message_queue"); // erase Previous Message Queue 
-  boost::interprocess::message_queue MQ(boost::interprocess::create_only, // create message queue
-                                        "Message_queue", // name
-                                        1,// max message number
-                                        4// max message size
-                                       );
-  int received_number = 0;
-  unsigned int priority;
-  boost::interprocess::message_queue::size_type received_size;
-  MQ.receive (&received_number, sizeof (received_number), received_size, priority);
-  std::cout << "received_number = " << received_number 
-            << ", received_size = " << received_size 
-            << ", priority = " << priority << std::endl;
-
-  std::string line;
-  while (std::getline(is, line))
+public:
+  void start()
   {
-    std::cout << line << std::endl;
-    if (line.find("END test") != std::string::npos)
+    std::string command = "./Prog ";
+    boost::thread parser(&Server::parseProg, this);
+    boost::process::child c(command.c_str(), boost::process::std_out > is);
+
+    c.detach();
+
+    boost::interprocess::message_queue::remove("Message_queue");            // erase Previous Message Queue
+    boost::interprocess::message_queue MQ(boost::interprocess::create_only, // create message queue
+                                          "Message_queue",                  // name
+                                          1,                                // max message number
+                                          4                                 // max message size
+    );
+    int received_number = 0;
+    unsigned int priority;
+    boost::interprocess::message_queue::size_type received_size;
+    MQ.receive(&received_number, sizeof(received_number), received_size, priority);
+    std::cout << "received_number = " << received_number
+              << ", received_size = " << received_size
+              << ", priority = " << priority << std::endl;
+  }
+
+private:
+  void parseProg()
+  {
+    std::string line;
+    while (std::getline(is, line))
     {
-      break;
+      std::cout << line << std::endl;
+      if (line.find("END test") != std::string::npos)
+      {
+        break;
+      }
     }
   }
-}
+
+  boost::process::ipstream is;
+};
 
 int main()
 {
@@ -51,12 +63,11 @@ int main()
 
     if (command == "0" || command == "start")
     {
-      std::cout << "start" << std::endl;
-      start();
+      Server server;
+      server.start();
     }
     else if (command == "1" || command == "stop")
     {
-      std::cout << "stop" << std::endl;
       break;
     }
   }
